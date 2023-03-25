4ad7da7095096e219e5dd029755d1576197f79af
jonathanpin <jonathan.chacon@catolica.edu.sv>
2023-03-25 11:20:37 -0600

    Agregar archivos de proyecto.

---
+++ practicaMVC05/Models/equiposDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace practicaMVC05.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<marcas> marcas { get; set; }
+    }
+}
